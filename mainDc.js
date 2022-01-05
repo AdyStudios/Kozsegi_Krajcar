5c19649911145b176dab92ab832697f5279db5a5
AdyStudios <karaadamka@gmail.com>
2022-01-05 16:32:07 +0100

    Upgrade

--- mainDc.js
+++ mainDc.js
@@ -1,11 +1,12 @@
 //make a discord bot that can respond to direct messages
-var discord = require('discord.js');
-var bot = new discord.Client();
+const Discord = require('discord.js');
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 var token = "OTI4MzAzMDcwNTA3NTg5NzQ0.YdWzmw.2QnWT_MPS0ri09GpkuQg_RP3J_Y";
 var prefix = "!";
 
-bot.login(token);
-console.log(`Logged in as ${client.user.tag}!`);
+
+console.log(`Logged in as !`);
 //listen for direct messages to the bot
 client.on("message", msg =>  { 
     
@@ -23,3 +24,5 @@ client.on('message', msg => {
         msg.reply('Pong!');
     }
 });
+
+client.login("OTI4MzAzMDcwNTA3NTg5NzQ0.YdWzmw.KNG8wGvooE7Hb90p6ZCmVP-KtjE");
