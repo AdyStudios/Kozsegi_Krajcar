4561b0611d45c411768d064a49ba532db32156a2
Hema2 <49586027+Hema2-official@users.noreply.github.com>
2022-01-07 16:30:23 +0100

    everything wurks out as expected sooo... here ya' go

--- networkpost.js
+++ networkpost.js
@@ -13,10 +13,47 @@ const wss = new WebSocket.Server({
 var server = http.createServer(function(req, res) {
     const method = req.method.toLowerCase();
     if (method === 'get') {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        fs.createReadStream(__dirname + '/index.html').pipe(res);
+        console.log('GET request received');
+        if (req.url.startsWith('/view')) {
+            //fetch users
+            var usersRaw = fs.readFileSync('./users.json');
+            var users = JSON.parse(usersRaw);
 
+            //get user from url
+            var user = req.url.split('?')[1];
+
+            //get user's cr
+            var cr = 0;
+            var success = false;
+            for (var i = 0; i < users.length; i++) {
+                if (user === users[i].username) {
+                    cr = users[i].cr;
+                    success = true;
+                }
+            }
+
+            //send response if successful
+            if (success) {
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                var html = fs.readFileSync(__dirname + '/index.html', 'utf8');
+                html = html.replace('%%%Name%%%', user);
+                html = html.replace('%%%Value%%%', cr);
+                res.end(html);
+                return;
+            }
+
+        } else if (req.url === ('/')) {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            fs.createReadStream(__dirname + '/chooser.html').pipe(res);
+            return;
+        }
+
+        //send error if unsuccessful
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        var html = fs.readFileSync(__dirname + '/404.html', 'utf8');
+        res.end(html);
     }
+
     res.writeHead(404);
     res.end();
 });
