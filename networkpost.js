0cb20b6a0a8ccc9aa7833e9c4da8a56a01032cc1
Hema2 <49586027+Hema2-official@users.noreply.github.com>
2022-01-07 13:15:43 +0100

    hmm

--- networkpost.js
+++ networkpost.js
@@ -1,8 +1,34 @@
-//run http server
-var http = require('http');
-var fs = require('fs');
-var server = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    fs.createReadStream(__dirname + '/index.html').pipe(res);
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const WebSocket = require('ws');
+
+const HTTP_PORT = 8089;
+const WEBSOCKET_PORT = 8090;
+
+const wss = new WebSocket.Server({
+    port: WEBSOCKET_PORT
 });
-server.listen(8080);
+
+var server = http.createServer(function(req, res) {
+    const method = req.method.toLowerCase();
+    if (method === 'get') {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + '/index.html').pipe(res);
+
+    }
+    res.writeHead(404);
+    res.end();
+});
+
+// Listen for file content changes in users.json
+fs.watchFile(path.join(__dirname, 'users.json'), function(curr, prev) {
+    console.log('File changed');
+    wss.clients.forEach(function(client) {
+        //close connnection with client
+        client.close();
+    });
+});
+console.log('Watching for file changes');
+
+server.listen(HTTP_PORT);
