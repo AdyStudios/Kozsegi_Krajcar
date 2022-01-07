677a0c999d3efd2cc5a1942d90a5e5bb7fc5ea63
Hema2 <49586027+Hema2-official@users.noreply.github.com>
2022-01-07 10:07:08 +0100

    http

--- networkpost.js
+++ networkpost.js
@@ -1,1 +1,8 @@
-//handle
+//run http server
+var http = require('http');
+var fs = require('fs');
+var server = http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.createReadStream(__dirname + '/index.html').pipe(res);
+});
+server.listen(8080);
