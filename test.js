df4fc31985df5b6e67c307fe4c084f2ca6871951
AdyStudios <karaadamka@gmail.com>
2022-01-14 10:35:38 +0100

    users save update

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 //push the users.json file changes to the github repository
 function pushChanges() {
     var exec = require('child_process').exec;
-    exec('git add . && git status && git commit -m "users save update" && git push', function (error, stdout, stderr) {
+    exec('git add . && git status && git commit -m "users save update" && git push -u origin master', function (error, stdout, stderr) {
         console.log(stdout);
         console.log(stderr);
         if (error !== null) {
