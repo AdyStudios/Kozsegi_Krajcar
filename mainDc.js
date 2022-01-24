13b8eaee34ee4aaab1196737f9bdde1f61fa7dd1
AdyStudios <karaadamka@gmail.com>
2022-01-24 17:43:08 +0100

    heroku (a html = 🤮)

--- mainDc.js
+++ mainDc.js
@@ -5,7 +5,7 @@ const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const indexFile = require('./index.js');
 var prefix = "!";
-let token = fs.readFileSync('./token.token', 'utf8');
+let token = process.env.token;
 client.commands = new Discord.Collection();
 
 
